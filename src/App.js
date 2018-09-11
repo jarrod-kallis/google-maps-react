461c8c8b58064f2c4259c5d7ae9efbc7b17a9b15
Jarrod Kallis <jarrod.kallis@altron.com>
2018-09-11 09:32:52 +0200

    Polygons

--- src/App.js
+++ src/App.js
@@ -13,18 +13,22 @@ class App extends Component {
   }
 
   componentDidMount() {
-    api.map.points.get().then(points => this.setState({ points }));
+    api.map.points.get().then(points => {
+      this.setState({ points })
+    });
   }
 
   render() {
     return (
       <div>
         <Map
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyByWkAe9UopNpFK6TPEEJ4ak4fMtS7IZD8&amp;v=3.exp&amp;libraries=geometry,drawing,places,visualization"
           loadingElement={<div style={{ height: `100vh` }} />}
           containerElement={<div style={{ height: `100vh` }} />}
           mapElement={<div style={{ height: `100vh` }} />}
+          isMarkerShown
           polygon={this.state.points}
+        // heatmap={this.state.points}
         />
       </div>
     );
--- src/Map.js
+++ src/Map.js
@@ -6,29 +6,44 @@ import {
   Marker,
   Polygon
 } from 'react-google-maps';
+// import HeatmapLayer from "react-google-maps/lib/components/visualization/HeatmapLayer";
 
 class Map extends React.Component {
   state = {};
 
   componentDidMount() {
     const bounds = new window.google.maps.LatLngBounds();
-    this.props.polygon.map(p =>
-      bounds.extend(new window.google.maps.LatLng(p.lat, p.lng))
-    );
+
+    if (this.props.polygon) {
+      this.props.polygon.map(p1 =>
+        p1.map(p => bounds.extend(new window.google.maps.LatLng(p.lat, p.lng)))
+      );
+    }
+
     this.refs.map.fitBounds(bounds);
   }
 
   render() {
+    // const heatmap = this.props.heatmap ? this.props.heatmap.map(point => new window.google.maps.LatLng(point.lat, point.lng)) : [];
+    const polygons = this.props.polygon ? this.props.polygon.map(p =>
+      <Polygon key={Math.random()}
+        paths={p}
+        options={{ fillColor: "#FF0000", fillOpacity: 0.2, strokeColor: "blue", strokeWeight: 0.1 }}
+        onClick={() => console.log('hi')}
+      />) : [];
+
     return (
       <GoogleMap
         ref="map"
-        // defaultZoom={8}
-        // defaultCenter={{ lat: 25.774, lng: -80.19 }}
+      // defaultZoom={8}
+      // defaultCenter={{ lat: 25.774, lng: -80.19 }}
       >
         {this.props.isMarkerShown && (
-          <Marker position={{ lat: 25.774, lng: -80.19 }} />
+          <Marker position={{ lng: 25.37236506583633, lat: -26.46498263403906 }} />
         )}
-        {this.props.polygon && <Polygon paths={this.props.polygon} />}
+        {polygons}
+        {/* {this.props.polygon && <Polygon paths={this.props.polygon} />} */}
+        {/* {heatmap && <HeatmapLayer data={heatmap} />} */}
       </GoogleMap>
     );
   }
